14f1c81c2c7e6d2e70011eb4c1a0deb7e0554c58
Mohamed jinas <boring-dragon@users.noreply.github.com>
2021-06-26 02:25:30 +0000

    Apply fixes from StyleCI

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,10 @@
 <?php
 
+use Baraveli\BmlOcr\BmlOcr;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use Baraveli\BmlOcr\BmlOcr;
 use Illuminate\Support\Facades\Storage;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -19,18 +20,16 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
 Route::post('/detect', function (Request $request) {
-
     $request->validate([
-        "name" => "required",
-        "data" => "required"
+        'name' => 'required',
+        'data' => 'required',
     ]);
 
-    $filename = md5($request->name) . time() . '.' . pathinfo($request->name, PATHINFO_EXTENSION);
+    $filename = md5($request->name).time().'.'.pathinfo($request->name, PATHINFO_EXTENSION);
     Storage::disk('public')->put($filename, base64_decode($request->data));
 
-    $result = BmlOcr::make(storage_path("app/public/" . $filename), storage_path())
+    $result = BmlOcr::make(storage_path('app/public/'.$filename), storage_path())
         ->detect();
 
     Storage::disk('public')->delete($filename);
